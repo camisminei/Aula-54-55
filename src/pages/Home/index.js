7376295480f279380d15a3c6b19e6f8f421541be
camisminei <camisminei.ufms@gmail.com>
2023-06-29 07:30:35 -0400

    aula54 concluida

---
+++ src/pages/Home/index.js
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom";
+function Home() {
+    return (
+        <div>
+            <h1>Bem vindo a pagina home</h1>
+            <br />
+            <br />
+
+            <Link to="/sobre">Sobre</Link> <br />
+            <Link to="/contato">Contato</Link>
+        </div>
+    );
+}
+
+export default Home;
